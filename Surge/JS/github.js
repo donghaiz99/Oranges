8d1fca40a3afa1c1d94f15be626bb45ab07d5236
zhang <donghaiz99@gmail.com>
2020-07-03 13:46:49 +0800

    github.js

--- Surge/JS/github.js
+++ Surge/JS/github.js
@@ -17,7 +17,7 @@
  * 📌 如果希望监控Release，请切换至Release界面，直接复制URL填入；
  */
 
-let token = "784a03feb07989d3339dfa41c7eb41777436cbfa";
+let token = "fd77160772c41403b54d54eb7de1f1b6424c73a0";
 
 let repositories = [
     {
